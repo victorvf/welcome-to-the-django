4b05f546a92076f4811107ac42d2b5a6bcfd4e76
˜victorvf˜ <victorvitoria11@hotmail.com>
2021-03-29 20:41:01 -0300

    feature: speakers

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.template.defaultfilters import safe
 
-# Register your models here.
+from eventex.core.models import Speaker
+
+
+class SpeakerModelAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name", "photo_img", "website_link"]
+
+    def website_link(self, obj):
+        return safe(f'<a href="{obj.website}">{obj.website}</a>')
+
+    website_link.short_description = "website"
+
+    def photo_img(self, obj):
+        return safe(f'<img style="width:32px;border-radius:100%;" src="{obj.photo}" />')
+
+    photo_img.short_description = "foto"
+
+
+admin.site.register(Speaker, SpeakerModelAdmin)
