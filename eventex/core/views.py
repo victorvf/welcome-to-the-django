07cae9f6ee64676f8ad75157adbd579275f79ec1
˜victorvf˜ <victorvitoria11@hotmail.com>
2021-04-16 20:30:21 -0300

    feature: fim do curso

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,26 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
-from eventex.core.models import Speaker, Talk, Course
-
-
-def home(request):
-    speakers = Speaker.objects.all()
-    return render(request, "core/index.html", {"speakers": speakers})
+from eventex.core.models import Speaker, Talk
 
 
-def speaker_detail(request, slug):
-    speaker = get_object_or_404(Speaker, slug=slug)
-    return render(request, "core/speaker_detail.html", {"speaker": speaker})
+home = ListView.as_view(
+    model=Speaker,
+    template_name="core/index.html",
+)
 
 
-def talk_list(request):
-    context = {
-        "morning_talks": Talk.objects.at_morning(),
-        "afternoon_talks": Talk.objects.at_afternoon(),
-    }
+speaker_detail = DetailView.as_view(model=Speaker)
 
-    return render(
-        request,
-        "core/talk_list.html",
-        context,
-    )
+
+talk_list = ListView.as_view(model=Talk)
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,66 +1,15 @@
-from django.http import HttpResponseRedirect, Http404
-from django.shortcuts import render, resolve_url as r
-from django.core import mail
-from django.template.loader import render_to_string
-from django.conf import settings
+from django.views.generic import DetailView
 
-from .forms import SubscriptionForm
-from .models import Subscription
+from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
+from eventex.subscriptions.mixins import EmailCreateView
 
 
-def new(request):
-    if request.method == "POST":
-        return create(request)
-
-    return empty_form(request)
-
-
-def create(request):
-    form = SubscriptionForm(request.POST)
-
-    if not form.is_valid():
-        return render(
-            request,
-            "subscriptions/subscription_form.html",
-            {"form": form},
-        )
-
-    subscription = form.save()
-
-    _send_mail(
-        "Confirmação de inscrição",
-        "subscriptions/subscription_email.txt",
-        settings.DEFAULT_FROM_EMAIL,
-        subscription.email,
-        {"subscription": subscription},
-    )
-
-    return HttpResponseRedirect(r("subscriptions:detail", subscription.pk))
+new = EmailCreateView.as_view(
+    model=Subscription,
+    form_class=SubscriptionForm,
+    email_subject="Confirmação de inscrição",
+)
 
 
-def empty_form(request):
-    return render(
-        request,
-        "subscriptions/subscription_form.html",
-        {"form": SubscriptionForm()},
-    )
-
-
-def detail(request, pk):
-    # get_object_or_404()
-    try:
-        subscription = Subscription.objects.get(pk=pk)
-    except Subscription.DoesNotExist:
-        raise Http404()
-
-    return render(
-        request,
-        "subscriptions/subscription_detail.html",
-        {"subscription": subscription}
-    )
-
-
-def _send_mail(subject, template_name, from_, to, context):
-    body = render_to_string(template_name, context)
-
-    mail.send_mail(subject, body, from_, [from_, to])
+detail = DetailView.as_view(model=Subscription)
